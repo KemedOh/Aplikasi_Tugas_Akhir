9c2eda5721d1219355ad27956b35d1188103ac68
KemedOh <fileahmadsiohgmail.com>
2025-04-29 20:33:30 +0700

    Update penyempurnaan tahap akhir aplikasi

--- app/Exports/RecommendationsExport.php
+++ app/Exports/RecommendationsExport.php
@@ -8,20 +8,21 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 
 class RecommendationsExport implements FromCollection, WithHeadings
 {
-    public function collection()
-    {
-        $users = User::with('recommendations.major')->get();
+   public function collection()
+{
+    $users = User::with('recommendations.major')->get();
 
-        return $users->map(function ($user) {
-            return [
-                'Nama' => $user->name,
-                'Email' => $user->email,
-                'Email' => $user->nomor_telepon,
-                'Sangat Direkomendasikan' => $user->recommendations->where('level', 'sangat_direkomendasikan')->first()?->major?->name ?? '-',
-                'Cukup Direkomendasikan' => $user->recommendations->where('level', 'cukup_direkomendasikan')->first()?->major?->name ?? '-',
-            ];
-        });
-    }
+    return $users->map(function ($user) {
+        return [
+            'Nama' => $user->name,
+            'Email' => $user->email,
+            'Nomor Telepon' => $user->nomor_telepon, // perbaikan di sini
+            'Sangat Direkomendasikan' => $user->recommendations->where('level', 'sangat_direkomendasikan')->first()?->major?->name ?? '-',
+            'Cukup Direkomendasikan' => $user->recommendations->where('level', 'cukup_direkomendasikan')->first()?->major?->name ?? '-',
+        ];
+    });
+}
+
 
     public function headings(): array
     {
